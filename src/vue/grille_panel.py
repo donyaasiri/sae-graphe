4fff8929cf02f3241dfdd68ee0c3e955209059e7
Menny206 <manalelb24@gmail.com>
2026-06-13 20:05:22 +0200

    Amelioration de l affichage de la grille

--- src/vue/grille_panel.py
+++ src/vue/grille_panel.py
@@ -1,3 +1,7 @@
+# -----------------------------------------------------------------------------
+# --- grille_panel.py ----------------------------------------------------------
+# -----------------------------------------------------------------------------
+# Classe GrillePanel pour le jeu Néonaure.
 #
 # Cette classe représente le panneau qui affiche toute la grille.
 #
@@ -15,129 +19,143 @@ from PyQt6.QtWidgets import QWidget, QGridLayout
 
 from src.vue.case_vue import CaseVue
 
+
 # -----------------------------------------------------------------------------
 # --- classe GrillePanel -------------------------------------------------------
 # -----------------------------------------------------------------------------
-
 class GrillePanel(QWidget):
     """
-Représente le panneau graphique contenant toutes les cases de la grille.
-"""
-
-# -------------------------------------------------------------------------
-# --- constructeur ---------------------------------------------------------
-# -------------------------------------------------------------------------
-def __init__(self) -> None:
-
-    # appel du constructeur de QWidget
-    super().__init__()
-
-    # création du layout en grille
-    self.layout = QGridLayout()
-
-    # suppression de l'espace entre les cases
-    self.layout.setSpacing(0)
-
-    # suppression des marges autour de la grille
-    self.layout.setContentsMargins(0, 0, 0, 0)
-
-    # application du layout au widget
-    self.setLayout(self.layout)
-
-# -------------------------------------------------------------------------
-# --- affichage de la grille ------------------------------------------------
-# -------------------------------------------------------------------------
-def afficher_grille(
-    self,
-    nb_lignes: int,
-    nb_colonnes: int,
-    valeurs: list,
-    fixes: list,
-    motifs: list,
-    callback_modification
-) -> None:
-    """
-    Affiche une grille à partir des données envoyées par la fenêtre principale.
+    Représente le panneau graphique contenant toutes les cases de la grille.
     """
 
-    # on supprime l'ancienne grille avant d'afficher la nouvelle
-    self.nettoyer_grille()
+    # -------------------------------------------------------------------------
+    # --- constructeur ---------------------------------------------------------
+    # -------------------------------------------------------------------------
+    def __init__(self) -> None:
 
-    # taille d'une case en pixels
-    taille_case: int = 48
+        # appel du constructeur de QWidget
+        super().__init__()
 
-    # taille totale du panneau
-    self.setFixedSize(nb_colonnes * taille_case, nb_lignes * taille_case)
+        # création du layout en grille
+        self.layout = QGridLayout()
 
-    # parcours des lignes
-    for y in range(nb_lignes):
+        # suppression de l'espace entre les cases
+        self.layout.setSpacing(0)
 
-        # parcours des colonnes
-        for x in range(nb_colonnes):
+        # suppression des marges autour de la grille
+        self.layout.setContentsMargins(0, 0, 0, 0)
 
-            # création de la case graphique
-            case_vue = CaseVue(y, x, callback_modification)
+        # application du layout au widget
+        self.setLayout(self.layout)
 
-            # affichage de la valeur
-            case_vue.set_valeur(valeurs[y][x])
+        # -------------------------------------------------------------------------
+    # --- affichage de la grille ------------------------------------------------
+    # -------------------------------------------------------------------------
+    def afficher_grille(
+        self,
+        nb_lignes: int,
+        nb_colonnes: int,
+        valeurs: list,
+        fixes: list,
+        motifs: list,
+        callback_modification
+    ) -> None:
+        """
+        Affiche une grille à partir des données envoyées par la fenêtre principale.
+        """
 
-            # état fixe ou modifiable
-            case_vue.set_fixe(fixes[y][x])
+        # on supprime l'ancienne grille
+        self.nettoyer_grille()
 
-            # identifiant du motif actuel
-            motif_actuel = motifs[y][x]
+        # taille d'une case
+        taille_case: int = 48
+        self.setFixedSize(nb_colonnes * taille_case, nb_lignes * taille_case)
 
-            # -----------------------------------------------------------------
-            # --- calcul des bordures pour rendre les motifs visibles ----------
-            # -----------------------------------------------------------------
+        # ---------------------------------------------------------------------
+        # --- calcul de la taille de chaque motif ------------------------------
+        # ---------------------------------------------------------------------
+        tailles_motifs: dict = {}
 
-            # bordure du haut
-            if y == 0 or motifs[y - 1][x] != motif_actuel:
-                haut = 3
-            else:
-                haut = 1
+        for y in range(nb_lignes):
+            for x in range(nb_colonnes):
 
-            # bordure du bas
-            if y == nb_lignes - 1 or motifs[y + 1][x] != motif_actuel:
-                bas = 3
-            else:
-                bas = 1
+                identifiant_motif = motifs[y][x]
 
-            # bordure de gauche
-            if x == 0 or motifs[y][x - 1] != motif_actuel:
-                gauche = 3
-            else:
-                gauche = 1
+                if identifiant_motif not in tailles_motifs:
+                    tailles_motifs[identifiant_motif] = 0
 
-            # bordure de droite
-            if x == nb_colonnes - 1 or motifs[y][x + 1] != motif_actuel:
-                droite = 3
-            else:
-                droite = 1
+                tailles_motifs[identifiant_motif] += 1
 
-            # application du style de la case
-            case_vue.appliquer_style(haut, droite, bas, gauche)
+        # ---------------------------------------------------------------------
+        # --- création des cases graphiques ------------------------------------
+        # ---------------------------------------------------------------------
+        for y in range(nb_lignes):
+            for x in range(nb_colonnes):
 
-            # ajout de la case dans le layout
-            self.layout.addWidget(case_vue, y, x)
+                # motif de la case actuelle
+                motif_actuel = motifs[y][x]
 
-# -------------------------------------------------------------------------
-# --- nettoyage de la grille -----------------------------------------------
-# -------------------------------------------------------------------------
-def nettoyer_grille(self) -> None:
-    """
-    Supprime toutes les cases actuellement affichées.
-    """
+                # valeur maximale = nombre de cases dans le motif
+                valeur_max = tailles_motifs[motif_actuel]
 
-    # tant qu'il reste des éléments dans le layout
-    while self.layout.count():
+                # création de la case graphique
+                case_vue = CaseVue(y, x, valeur_max, callback_modification)
 
-        # on récupère le premier élément
-        item = self.layout.takeAt(0)
+                # affichage de la valeur
+                case_vue.set_valeur(valeurs[y][x])
 
-        # on récupère le widget contenu dans l'élément
-        widget = item.widget()
+                # état fixe ou modifiable
+                case_vue.set_fixe(fixes[y][x])
 
-        # si un widget existe, on le supprime
-        if widget is not None:
-            widget.deleteLater()
+                # bordure du haut
+                if y == 0 or motifs[y - 1][x] != motif_actuel:
+                    haut = 3
+                else:
+                    haut = 1
+
+                # bordure du bas
+                if y == nb_lignes - 1 or motifs[y + 1][x] != motif_actuel:
+                    bas = 3
+                else:
+                    bas = 1
+
+                # bordure de gauche
+                if x == 0 or motifs[y][x - 1] != motif_actuel:
+                    gauche = 3
+                else:
+                    gauche = 1
+
+                # bordure de droite
+                if x == nb_colonnes - 1 or motifs[y][x + 1] != motif_actuel:
+                    droite = 3
+                else:
+                    droite = 1
+
+                # application du style
+                case_vue.appliquer_style(haut, droite, bas, gauche)
+
+                # ajout de la case dans le layout
+                self.layout.addWidget(case_vue, y, x)
+
+    # -------------------------------------------------------------------------
+    # --- nettoyage de la grille -----------------------------------------------
+    # -------------------------------------------------------------------------
+    def nettoyer_grille(self) -> None:
+        """
+        Supprime toutes les cases actuellement affichées.
+        """
+
+        # tant qu'il reste des éléments dans le layout
+        while self.layout.count():
+
+            # on récupère le premier élément
+            item = self.layout.takeAt(0)
+
+            # on récupère le widget contenu dans l'élément
+            widget = item.widget()
+
+            # si un widget existe, on le supprime
+            if widget is not None:
+                widget.deleteLater()
+
+
